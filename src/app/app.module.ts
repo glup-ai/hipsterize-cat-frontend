f0f7dcbc71c897e72dc983ddb04db4172c4e1266
Målfrid Henden Aaraas <malfridha@gmail.com>
2021-08-17 20:30:36 +0200

    add declarations and import to add module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatButtonModule } from '@angular/material/button';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+
+import { DragAndDropFileComponent } from './drag-and-drop-file/drag-and-drop-file.component';
+import { DragAndDropFileDirective } from './drag-and-drop-file/drag-and-drop-file.directive';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    DragAndDropFileComponent,
+    DragAndDropFileDirective
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
