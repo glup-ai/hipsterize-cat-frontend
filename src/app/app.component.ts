44d4ee07811be5bd0d73f13bc1b4a72fcf189a80
Målfrid Henden Aaraas <malfridha@gmail.com>
2021-08-17 20:30:11 +0200

    remove code delegated to other component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HipsterizecatService } from './services/hipsterizecat.service';
 
 @Component({
   selector: 'app-root',
@@ -7,40 +6,7 @@ import { HipsterizecatService } from './services/hipsterizecat.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Hipsterize your cat';
-  catFile: File | null = null;
-  shadesFile: File | null = null;
-  hipsterizedCat: File | null = null;
-
-  constructor(private hipsterizeCatService: HipsterizecatService){}
-
-  uploadShades(event: any) {
-    if(event){
-      this.shadesFile = event.target.files.item(0);
-      console.log(this.shadesFile);
-    }
-  }
-
-  uploadCat(event: any) {
-    if(event){
-      this.catFile = event.target.files.item(0);
-      console.log(this.catFile);
-    }
-  }
-
-  hipsterizeCat(){
-    console.log('Click...')
-    if(this.catFile && this.shadesFile){
-      this.hipsterizeCatService.post(this.shadesFile, this.catFile).subscribe(
-        result => {
-          this.hipsterizedCat = result;
-          console.log(this.hipsterizedCat);
-        },
-        error => {
-          console.log(error);
-        }
-      )
-    }
-  }
+  
+  constructor(){}
 
 }
