bb94c04c935aa2cc6e067b08db40f05d8800e66f
Målfrid Henden Aaraas <malfridha@gmail.com>
2021-11-15 18:48:29 +0100

    cleanup unused code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-
 import { MatButtonModule } from '@angular/material/button';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-
-
 import { DragAndDropFileComponent } from './drag-and-drop-file/drag-and-drop-file.component';
 import { DragAndDropFileDirective } from './drag-and-drop-file/drag-and-drop-file.directive';
 
