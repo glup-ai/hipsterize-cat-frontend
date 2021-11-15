0893bc44141faebbee5d7e2157f7da68b0b4373b
Målfrid Henden Aaraas <malfridha@gmail.com>
2021-11-15 18:47:29 +0100

    display api result for hipsterized cat

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { HipsterizecatService } from '../services/hipsterizecat.service';
+import { HttpClient } from '@angular/common/http';
+import { EMPTY, Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -11,31 +14,48 @@ export class HomeComponent{
   title = 'Hipsterize your cat';
   catFile: File | null = null;
   shadesFile: File | null = null;
-  hipsterizedCat: File | null = null;
+  hipsterizedCat: any = null;
+  hipsterizedCatImageSrc: SafeUrl | null;
 
-  constructor(private hipsterizeCatService: HipsterizecatService){}
-
-  uploadShades(file: File) {
-    this.shadesFile = file;
+  constructor(private http: HttpClient, private domSanitizer: DomSanitizer){
+    this.hipsterizedCatImageSrc = null;
   }
 
-  uploadCat(file: File) {
-    this.catFile = file;
-  }
-
-  hipsterizeCat(){
-    console.log('Click...')
+  public hipsterizeCat(){
     if(this.catFile && this.shadesFile){
-      this.hipsterizeCatService.post(this.shadesFile, this.catFile).subscribe(
-        result => {
-          this.hipsterizedCat = result;
-          console.log(this.hipsterizedCat);
-        },
-        error => {
-          console.log(error);
+      this.getImage(this.shadesFile, this.catFile)
+        .pipe(catchError((error: Error) => throwError(error)))
+        .subscribe((response: ArrayBuffer) => {
+          let array = new Uint8Array(response);
+          let stringChar = array.reduce((data, byte) => {
+            return data + String.fromCharCode(byte);
+          }, '');
+          let base64string = btoa(stringChar);
+          this.hipsterizedCatImageSrc = this.domSanitizer.bypassSecurityTrustUrl(
+            'data:image/jpg;base64, ' + base64string
+          );
         }
       )
     }
+  }
+
+  public reset(){
+    this.catFile = null; 
+    this.shadesFile = null; 
+    this.hipsterizedCat = null; 
+    this.hipsterizedCatImageSrc = null;
+  }
+
+  public getImage(shadesFile: File, catFile: File): Observable<any> {
+    var formData = new FormData();
+    formData.append("shades", shadesFile, shadesFile.name);
+    formData.append("cat", catFile, shadesFile.name);
+    
+    return this.http.post(
+      "https://glup-fa.azurewebsites.net/api/gluphttptrigger",
+      formData,
+      { responseType: 'arraybuffer' }  // 'arraybuffer'|'blob'|'json'|'text'
+    );
   }
 
 }
